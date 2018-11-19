e57a9561e7979a904dc5674c82f1f80141e8532a
Um Sinan <usinanoglu@hotmail.com>
2018-11-19 22:02:57 +0300

    Starting 19.11.2018 22:02

---
+++ GeckoFX_Test03/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Gecko;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeckoFX_Test03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Xpcom.Initialize("Firefox");
+            var geckoWebBrowser = new GeckoWebBrowser { Dock = DockStyle.Fill };
+            //Form f = new Form();
+            geckoWebBrowser1.Controls.Add(geckoWebBrowser);
+            geckoWebBrowser.Navigate("www.google.com");
+            //Application.Run(f);
+        }
+    }
+}
---
+++ GeckoFX_Test03/Program.cs
@@ -0,0 +1,26 @@
+﻿using Gecko;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeckoFX_Test03
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+
+        }
+    }
+}
